cda2163530a52402e023ff2dc9c035ed538db3bb
dilyar118 <daerken118@gmail.com>
2019-12-20 23:06:11 -0500

    new login page

--- src/test/java/com/odoo/Pages/LoginPage.java
+++ src/test/java/com/odoo/Pages/LoginPage.java
@@ -1,12 +1,14 @@
 package com.odoo.Pages;
 
+import com.odoo.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+
 public class LoginPage {
 
-    public LoginPage(){ PageFactory.initElemnts(Driver.get(), this); }
+    public LoginPage(){ PageFactory.initElements(Driver.get(), this); }
 
     @FindBy(id = "login")
     public WebElement username;
---
+++ src/test/java/com/odoo/utilities/BrowserUtils.java
@@ -0,0 +1,36 @@
+package com.odoo.utilities;
+
+import org.junit.Assert;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrowserUtils {
+
+    /**
+     * Switches to new window by the exact title. Returns to original window if target title not found
+     *
+     * @param targetTitle
+     */
+    public static void switchToWindow(String targetTitle) {
+        String origin = Driver.get().getWindowHandle();
+        for (String handle : Driver.get().getWindowHandles()) {
+            Driver.get().switchTo().window(handle);
+            if (Driver.get().getTitle().equals(targetTitle)) {
+                return;
+            }
+        }
+        Driver.get().switchTo().window(origin);
+    }
+}
+
+/**
+ * Moves the mouse to given element
+ *
+ * @param element on which to hover
+ */
