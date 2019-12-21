66b79705c445cadea7a712a1202432a2f89307e8
dilyar118 <daerken118@gmail.com>
2019-12-21 18:18:48 -0500

    BasePage

--- src/test/java/com/odoo/Pages/BasePage.java
+++ src/test/java/com/odoo/Pages/BasePage.java
@@ -1,117 +1,57 @@
 package com.odoo.Pages;
 
-import com.odoo.utilities.BrowserUtils;
+
 import com.odoo.utilities.Driver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.util.List;
 
 public abstract class BasePage {
 
-    @FindBy(css = "span.title-level-1")
+    @FindBy(xpath = "//li[@style='display: block;']")
     public List<WebElement> menuOptions;
 
-    @FindBy(css = "div[class='loader-mask shown']")
-    @CacheLookup
-    protected WebElement loaderMask;
+    @FindBy(xpath = "//input[@class='o_searchview_input']")
+    public WebElement searchInput;
 
-    @FindBy(css = "h1[class='oro-subtitle']")
-    public WebElement pageSubTitle;
+    @FindBy(xpath = "(//span[@class='o_channel_name'])[1]")
+    public WebElement inbox;
 
-    @FindBy(css = "#user-menu > a")
-    public WebElement userName;
+    @FindBy(xpath = "(//span[@class='o_channel_name'])[2]")
+    public WebElement starred;
 
-    @FindBy(linkText = "Logout")
-    public WebElement logOutLink;
+    @FindBy(xpath = "(//span[@class='o_channel_name'])[3]")
+    public WebElement general;
 
-    @FindBy(linkText = "My User")
-    public WebElement myUser;
+    @FindBy(xpath = "(//span[@class='o_channel_name'])[4]")
+    public WebElement projectAndTasks;
 
-    public BasePage() {
+    @FindBy(xpath = "//h4[@class='o_mail_open_channels']")
+    public WebElement channels;
+
+    @FindBy(xpath = "//div[@class='o_mail_sidebar_title']")
+    public List<WebElement> mailSidebars;
+
+    @FindBy(xpath = "//span[@title='Add']")
+    public List<WebElement> addButtons;
+
+    @FindBy(xpath = "//span[@title='Advanced Search...']")
+    public WebElement advancedSearch;
+
+    public BasePage(){
         PageFactory.initElements(Driver.get(), this);
     }
 
 
-    /**
-     * @return page name, for example: Dashboard
-     */
-    public String getPageSubTitle() {
-        //ant time we are verifying page name, or page subtitle, loader mask appears
-        waitUntilLoaderScreenDisappear();
-//        BrowserUtils.waitForStaleElement(pageSubTitle);
-        return pageSubTitle.getText();
-    }
-
-
-    /**
-     * Waits until loader screen present. If loader screen will not pop up at all,
-     * NoSuchElementException will be handled  bu try/catch block
-     * Thus, we can continue in any case.
-     */
-    public void waitUntilLoaderScreenDisappear() {
-        try {
-            WebDriverWait wait = new WebDriverWait(Driver.get(), 5);
-            wait.until(ExpectedConditions.invisibilityOf(loaderMask));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public String getUserName(){
-        waitUntilLoaderScreenDisappear();
-        BrowserUtils.waitForVisibility(userName, 5);
-        return userName.getText();
-    }
 
 
 
-    public void logOut(){
-        BrowserUtils.waitFor(2);
-        BrowserUtils.clickWithJS(userName);
-        BrowserUtils.clickWithJS(logOutLink);
-    }
-    public void goToMyUser(){
-        waitUntilLoaderScreenDisappear();
-        BrowserUtils.waitForClickablility(userName, 5).click();
-        BrowserUtils.waitForClickablility(myUser, 5).click();
 
-    }
 
-    /**
-     * This method will navigate user to the specific module in vytrack application.
-     * For example: if tab is equals to Activities, and module equals to Calls,
-     * Then method will navigate user to this page: http://qa2.vytrack.com/call/
-     *
-     * @param tab
-     * @param module
-     */
-    public void navigateToModule(String tab, String module) {
-        String tabLocator = "//span[normalize-space()='" + tab + "' and contains(@class, 'title title-level-1')]";
-        String moduleLocator = "//span[normalize-space()='" + module + "' and contains(@class, 'title title-level-2')]";
-        try {
-            BrowserUtils.waitForClickablility(By.xpath(tabLocator), 5);
-            WebElement tabElement = Driver.get().findElement(By.xpath(tabLocator));
-            new Actions(Driver.get()).moveToElement(tabElement).pause(200).doubleClick(tabElement).build().perform();
-        } catch (Exception e) {
-            BrowserUtils.clickWithWait(By.xpath(tabLocator), 5);
-        }
-        try {
-            BrowserUtils.waitForPresenceOfElement(By.xpath(moduleLocator), 5);
-            BrowserUtils.waitForVisibility(By.xpath(moduleLocator), 5);
-            BrowserUtils.scrollToElement(Driver.get().findElement(By.xpath(moduleLocator)));
-            Driver.get().findElement(By.xpath(moduleLocator)).click();
-        } catch (Exception e) {
-//            BrowserUtils.waitForStaleElement(Driver.get().findElement(By.xpath(moduleLocator)));
-            BrowserUtils.clickWithTimeOut(Driver.get().findElement(By.xpath(moduleLocator)),  5);
-        }
-    }
+
+
 
 }
