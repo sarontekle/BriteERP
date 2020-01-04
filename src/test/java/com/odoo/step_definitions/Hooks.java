ef875245c9c126a6b8b27c416ed46e7d80ff65a4
hamedjan1 <hamedazim.123@gmail.com>
2020-01-04 00:39:27 -0800

    after rec

--- src/test/java/com/odoo/step_definitions/Hooks.java
+++ src/test/java/com/odoo/step_definitions/Hooks.java
@@ -10,32 +10,32 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
-//
-//    @Before
-//    public void setUp() throws InterruptedException {
-//        System.out.println("\nThis is coming from before scenario");
-//
-//
-//
-//    }
-//
-//    @After
-//    public void tearDown(Scenario scenario) {
-//        System.out.println("This is coming from after scenario\n");
-//        // if scenario fails, take a screenshot
-//        if (scenario.isFailed()) {
-//            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
-//            scenario.embed(screenshot, "image/png");
-//        }
-//
-//        Driver.closeDriver();
-//    }
-//
-//    @After("@sales_manager")
-//    public void tearDownSalesManager(){
-//        System.out.println("This is coming from after scenario for sales managers\n");
-//
-//    }
+
+    @Before
+    public void setUp() throws InterruptedException {
+        System.out.println("\nThis is coming from before scenario");
+
+
+
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        System.out.println("This is coming from after scenario\n");
+        // if scenario fails, take a screenshot
+        if (scenario.isFailed()) {
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
+
+        Driver.closeDriver();
+    }
+
+    @After("@sales_manager")
+    public void tearDownSalesManager(){
+        System.out.println("This is coming from after scenario for sales managers\n");
+
+    }
 
 
 }
--- src/test/resources/features/VendorsBillPage.feature
+++ src/test/resources/features/VendorsBillPage.feature
@@ -1,26 +1,11 @@
 @login
-Feature: As a pos manager, I should be able to login home page
+Feature: As a post manager, I should be able to see Vendor Bills page
 
   Scenario: Pos manager wants to log in
     Given  The pos manager is on the log in page
     When The pos manager enters the pos manager creditials
     Then The pos manager should be able to log in
-
-
-
-
-
-    @purchase
-  Scenario: As a pos manager, I should be  able to click in purchases page
-    Given The pos manager is on the dashboard page
     When the pos manager click on the purchase tab
     Then the pos manager should be able to navigate to purchase page
-
-
-
-
-  @vendor
-  Scenario:As a post manager, I should be able to see Vendor Bills page
-    Given The pos manager is on the purchase page
     When The pos manager clicks on the vendor bills tab on the left
     Then The pos manager should be able to naviagte to vendor bills page
