0b532f3322c267ebfa200198991f6ddeebfc33d9
dilyar118 <daerken118@gmail.com>
2020-01-10 17:54:17 -0500

    wfwfe

--- src/test/java/com/odoo/Pages/PurchasesPage.java
+++ src/test/java/com/odoo/Pages/PurchasesPage.java
@@ -3,7 +3,6 @@ package com.odoo.Pages;
 import com.odoo.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 
 public class PurchasesPage extends BasePage{
